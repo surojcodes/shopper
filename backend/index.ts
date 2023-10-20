3134358cf92d25ef37f3fc885dd86296f3dca0d2
Suroj M <surojcodes@gmail.com>
2023-10-20 09:41:20 -0500

    formatting

--- backend/index.ts
+++ backend/index.ts
@@ -7,18 +7,18 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const mongoUri = process.env.MONGO_URI;
 
-const startApp = async function (){
+const startApp = async function () {
   try {
-    if(!mongoUri){
-      throw new Error('Mongo Connection URL missing')
+    if (!mongoUri) {
+      throw new Error('Mongo Connection URL missing');
     }
-    await mongoose.connect(mongoUri)
+    await mongoose.connect(mongoUri);
     console.log('MongoDB Connected');
-  } catch (e:any) {
-    console.log('Could not connect to MongoDB:',e.message);
+  } catch (e: any) {
+    console.log('Could not connect to MongoDB:', e.message);
     process.exit();
   }
-  app.listen(port,()=>console.log(`App Listening on port ${port}...`))
-}
+  app.listen(port, () => console.log(`App Listening on port ${port}...`));
+};
 
 startApp();
